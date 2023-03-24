7c332f3b2cfb6fb4c2f380dd3151964821a3dfe7
droppss909 <ed282001@gmail.com>
2023-03-24 23:48:31 +0100

    firs commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from matrix import *
+
+
+if __name__=='__main__':
+    matrix=matrix()
+    matrix.get_data("D:\pythonProject4\9.200-40")
+    matrix.print_data()
+    matrix.do_matrix()
+    matrix.show_matrix()
+
