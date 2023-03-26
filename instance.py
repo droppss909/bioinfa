34b09885eb906a9aa05ec6ab149c88c0efb8f35b
Jan Majchrzak <jan.majchrzak@protonmail.com>
2023-03-26 17:18:05 +0200

    Implement sequence reconstructing

--- instance.py
+++ instance.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 
 class Instance:
@@ -12,8 +13,9 @@ class Instance:
         file = open(file_path, "r")
         for line in file:
             self.spectrum.append(line[:-1])
+        random.shuffle(self.spectrum)
         K = len(self.spectrum)
-        self.matrix = np.zeros((K, K))
+        self.matrix = np.zeros((K, K), dtype=int)
         self.l = len(self.spectrum[0])
         self.n = n
         self.k = len(self.spectrum)
@@ -26,7 +28,7 @@ class Instance:
                     continue
                 for i in range(1, self.l):
                     if self.spectrum[first_word][i:] == self.spectrum[second_word][:-i]:
-                        self.matrix[first_word][second_word] = i
+                        self.matrix[first_word][second_word] = self.l - i
                         break
 
     def get_adj(self, i, j):
--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 from instance import *
 
 
-def DFS(instance: Instance, v, used=None, currentPath=[], currentCost=0, currentLength=0):
+def DFS(instance: Instance, v, used=None, currentPath=[], currentCost=0, currentBestPath=[]):
     if used is None:
         used = set()
-        currentLength = instance.l
-    if currentLength >= instance.n:
-        # print(currentPath)
-        return
     used.add(v)
     currentPath.append(v)
+    if currentCost >= instance.n - instance.l:
+        return currentPath
     successors = [i for i in range(
         instance.k) if i not in used and instance.get_adj(v, i) > 0]
     successors = sorted(successors, key=lambda s: instance.get_adj(v, s))
-    print(instance.spectrum[v])
-    for successor in successors[:1]:
-        edgeCost = int(instance.get_adj(v, successor))
-        print(" "*(currentLength - instance.l + edgeCost), end="")
-        DFS(instance, successor, used, currentPath,
-            currentCost+edgeCost, currentLength+edgeCost)
+    for successor in successors[-1:]:
+        edgeCost = instance.l - int(instance.get_adj(v, successor))
+        result = DFS(instance, successor, used.copy(), currentPath.copy(),
+                     currentCost+edgeCost, currentBestPath.copy())
+        if len(result) > len(currentBestPath):
+            currentBestPath = result.copy()
+    return currentBestPath
+
+
+def reconstructSequence(path, spectrum, matrix):
+    sequence = spectrum[path[0]]
+    l = len(sequence)
+    globalOffset = 0
+    for i in range(1, len(path)):
+        offset = l - int(matrix[path[i-1]][path[i]])
+        sequence = sequence + (spectrum[path[i]][-offset:])
+    return sequence
 
 
 if __name__ == '__main__':
     instance = Instance("9.200-40", 209)
-    print(instance.matrix)
-    DFS(instance, 7)
+    longestPath = []
+    for i in range(instance.k):
+        path = DFS(instance, i, None, [], 0, [])
+        print(len(path))
+        if len(path) > len(longestPath):
+            longestPath = path
+    print(len(longestPath))
+    sequence = reconstructSequence(
+        longestPath, instance.spectrum, instance.matrix)
+    print(sequence)
+    print(len(sequence))
